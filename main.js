86cee461a0d4caf03b00135d4f1ee3b3b27ce465
letangileri <let.angileri@gmail.com>
2025-07-21 18:25:42 +0200

    update bonus

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 let buttonSwitchLamp = document.getElementById("switch-lamp");
 
-buttonSwitchLamp.addEventListener ('click', function (){
+// buttonSwitchLamp.addEventListener ('click', function (){
+//     let lampOff = document.getElementById('lamp');
+//     // console.log(lampOff.src);
+//     lampOff.src = "../img/yellow_lamp.png"    
+//     // console.log(lampOff.src);
+//     // console.log(buttonSwitchLamp.innerText);
+//     let buttonText = buttonSwitchLamp.innerText;
+//     // console.log(typeof buttonText);
+    
+//     if(buttonText.includes("Accendi")){
+//         // console.log(buttonText);
+//         // console.log(buttonSwitchLamp.textContent = "Spegni");  
+  
+//         buttonSwitchLamp.textContent = "Spegni";     
+//     } else {
+//         buttonSwitchLamp.textContent = "Accendi";     
+//         lampOff.src = "../img/white_lamp.png";
+//     }
+
+// })
+
+
+//il corpo della funzione è uguale -- funzione di callback anonima
+function getSwitch () {
     let lampOff = document.getElementById('lamp');
     // console.log(lampOff.src);
     lampOff.src = "../img/yellow_lamp.png"    
@@ -18,5 +41,6 @@ buttonSwitchLamp.addEventListener ('click', function (){
         buttonSwitchLamp.textContent = "Accendi";     
         lampOff.src = "../img/white_lamp.png";
     }
+}
 
-})
+buttonSwitchLamp.addEventListener('click', getSwitch);
